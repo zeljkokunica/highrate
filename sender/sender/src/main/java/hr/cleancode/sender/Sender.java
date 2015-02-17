f312672cc987e8c2b910bf0b8cff484c989da8e0
zac <zeljko.kunica@gmail.com>
2015-02-17 19:34:17 +0100

    simple test sender

--- sender/sender/src/main/java/hr/cleancode/sender/Sender.java
+++ sender/sender/src/main/java/hr/cleancode/sender/Sender.java
@@ -1,25 +1,37 @@
 package hr.cleancode.sender;
 
-import org.joda.time.DateTime;
-
-import java.util.Date;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
+import org.joda.time.DateTime;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.stereotype.Component;
+
 /**
  * Created by zac on 14/02/15.
  */
-public class MessageSender {
-
-	public static void main(String[] args) throws InterruptedException {
+@Component
+public class Sender {
+	public void run() {
 		DateTime start = DateTime.now();
 		ExecutorService executorService = Executors.newCachedThreadPool();
 		for (int i = 0; i < 10; i++) {
 			executorService.execute(new Thread(new MessageSenderThread(i, 100000L, "http://78.46.95.77:9090/")));
 		}
 		executorService.shutdown();
-		while(!executorService.awaitTermination(1L, TimeUnit.SECONDS));
+		try {
+			while(!executorService.awaitTermination(1L, TimeUnit.SECONDS));
+		}
+		catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 		System.out.println("Test sending took " + String.valueOf(DateTime.now().getMillis() - start.getMillis()) + "ms");
+	}
+
+	public static void main(String[] args) {
+		ApplicationContext ctx = new AnnotationConfigApplicationContext("hr.cleancode.sender");
+		ctx.getBean(Sender.class).run();
 	}
 }
