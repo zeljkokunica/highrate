a2780be41994fe127fa707fa9f171070a7707063
zac <zeljko.kunica@gmail.com>
2015-02-18 15:53:37 +0100

    logging

--- web/src/main/java/hr/cleancode/web/StatisticDispatcher.java
+++ web/src/main/java/hr/cleancode/web/StatisticDispatcher.java
@@ -2,6 +2,8 @@ package hr.cleancode.web;
 
 import hr.cleancode.domain.CompleteStatisticsWrapper;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 
@@ -9,12 +11,16 @@ import org.springframework.messaging.simp.SimpMessagingTemplate;
  * Created by zac on 15/02/15.
  */
 public class StatisticDispatcher {
+	private static final Logger logger = LoggerFactory.getLogger(StatisticDispatcher.class);
 	@Autowired
 	private SimpMessagingTemplate template;
 
 	public void receiveStatistic(Object object) {
 		if (object instanceof CompleteStatisticsWrapper) {
 			template.convertAndSend("/topic/stats", object);
+		}
+		else {
+			logger.error("Got wrong message!");
 		}
 	}
 
